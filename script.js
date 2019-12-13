6820eef95e1ac5c85a98b313948fc903325d2dfd
Noodles <charles.pitek@gmail.com>
2019-12-13 09:42:31 -0700

    added style.css and did some minor styling

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var password = ""
 var generate = document.getElementById("generate")
 function passwordGenerator() {
-
+    
     var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     var spcChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
     
+    var password = ""
     var x = 0
     var numSelect = 0
     var passwordChar = []
